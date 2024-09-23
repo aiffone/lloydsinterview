f84afe761658d4e021a4757db19fc3e15ec22d13
kayode <aiffone@google.com>
2024-09-23 17:21:31 +0000

    update project

--- bucket.tf
+++ bucket.tf
@@ -1,6 +1,6 @@
 # Terraform state bucket
 resource "google_storage_bucket" "terraform_state" {
-  name     = "state0001"
+  name     = "state00011"
   location = "US"
 
   versioning {
@@ -20,7 +20,7 @@ resource "google_storage_bucket" "terraform_state" {
 
 # Cloud Build logs bucket
 resource "google_storage_bucket" "cloudbuild_logs" {
-  name     = "cloudbuild-logs-0001"
+  name     = "cloudbuild-logs-00011"
   location = "US"
 
   versioning {
