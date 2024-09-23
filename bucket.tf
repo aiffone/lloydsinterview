e52111a9cd3dad893e486c9110b67ff78f27a524
kayode <aiffone@gmail.com>
2024-09-23 16:51:44 +0000

    creae bucket for build logs

--- bucket.tf
+++ bucket.tf
@@ -1,46 +1,41 @@
+# Terraform state bucket
 resource "google_storage_bucket" "terraform_state" {
   name     = "state0001"
   location = "US"
 
-  # Enable versioning for state bucket
   versioning {
     enabled = true
   }
 
-  # Set lifecycle rule for the state bucket
   lifecycle_rule {
     action {
       type = "Delete"
     }
 
     condition {
-      age = 365  # Adjust as needed for lifecycle policies
+      age = 365
     }
   }
 }
 
-# New bucket for Cloud Build logs
+# Cloud Build logs bucket
 resource "google_storage_bucket" "cloudbuild_logs" {
-  name     = "cloudbuild-logs-interview-0001"  # Ensure this name is unique in GCS
-  location = "US"  # Set to the appropriate location
+  name     = "cloudbuild-logs-0001"
+  location = "US"
 
-  # Optional settings for logs bucket
   versioning {
-    enabled = false  # Logs bucket doesn't need versioning
+    enabled = false
   }
 
-  # Optional lifecycle rule for log retention
   lifecycle_rule {
     action {
       type = "Delete"
     }
 
     condition {
-      age = 30  # Retain logs for 30 days, adjust as needed
+      age = 30
     }
   }
 
-  # Enable uniform access control for the bucket
   uniform_bucket_level_access = true
 }
-
