28821a1e097c0b6055b8310fd7214324dd28fe49
kayode <aiffone@google.com>
2024-09-26 14:12:05 +0100

    gke

--- gke.tf
+++ gke.tf
@@ -5,10 +5,28 @@ resource "google_container_cluster" "primary" {
   network    = google_compute_network.vpc_network.id
   subnetwork = google_compute_subnetwork.vpc_subnetwork.id
 
-  initial_node_count = 1
+  initial_node_count = 0  # Set to 0 as we are using a custom node pool
+
+  # Optional: Enable basic GKE features
+  enable_ip_alias = true
+  remove_default_node_pool = true  # We are creating our own node pool
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  cluster    = google_container_cluster.primary.name
+  location   = google_container_cluster.primary.location
+  name       = "${var.cluster_name}-node-pool"
+  
+  node_count = 2  # Start with 2 nodes
+
+  # Enable autoscaling
+  autoscaling {
+    min_node_count = 2
+    max_node_count = 4
+  }
 
   node_config {
-    machine_type = "e2-medium" # Increased from e2-micro to e2-medium
+    machine_type = "e2-medium"  # Increased from e2-micro to e2-medium
     disk_size_gb = 50
 
     oauth_scopes = [
