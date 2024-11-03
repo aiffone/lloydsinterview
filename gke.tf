94303950acd501b17a08fbb0c143bfd23e75127f
kayode <aiffone@google.com>
2024-11-03 03:48:15 +0000

    scale nodes

--- gke.tf
+++ gke.tf
@@ -1,19 +1,32 @@
+resource "google_container_cluster" "primary" {
+  name     = var.cluster_name
+  location = var.region
+
+  initial_node_count = 1
+
+  node_config {
+    machine_type = "e2-medium"
+    disk_size_gb = 10
+    disk_type    = "pd-standard"
+  }
+
+  remove_default_node_pool = true
+}
+
 resource "google_container_node_pool" "primary_nodes" {
-  cluster  = google_container_cluster.primary.name  # Reference the primary GKE cluster
+  cluster  = google_container_cluster.primary.name
   location = google_container_cluster.primary.location
   name     = "${var.cluster_name}-node-pool"
 
-  # Enable autoscaling for the node pool
   autoscaling {
     min_node_count = 1
-    max_node_count = 3  # Increase max nodes for more resources
+    max_node_count = 3
   }
 
-  # Node configuration for the custom node pool
   node_config {
-    machine_type = "e2-standard-2"  # Change to a type with more resources
-    disk_size_gb = 20  # Increase disk size if needed
-    disk_type    = "pd-standard"  # Use standard persistent disk
+    machine_type = "e2-standard-2"
+    disk_size_gb = 20
+    disk_type    = "pd-standard"
     oauth_scopes = [
       "https://www.googleapis.com/auth/devstorage.read_only",
       "https://www.googleapis.com/auth/logging.write",
