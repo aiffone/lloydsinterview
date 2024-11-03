e63e0d7f11c33cd4e2a8850f73b570f6ff736caa
kayode <aiffone@google.com>
2024-11-03 03:54:16 +0000

    scale nodes

--- gke.tf
+++ gke.tf
@@ -1,5 +1,22 @@
-resource "google_container_cluster" "primary" {
-  name     = var.cluster_name
+# Define variables for cluster names and region
+variable "region" {
+  description = "The GCP region to create the clusters in"
+  default     = "us-central1"
+}
+
+variable "infra1_cluster_name" {
+  description = "The name of the first cluster"
+  default     = "infra1"
+}
+
+variable "infra2_cluster_name" {
+  description = "The name of the second cluster"
+  default     = "infra2"
+}
+
+# Create the first GKE cluster
+resource "google_container_cluster" "infra1" {
+  name     = var.infra1_cluster_name
   location = var.region
 
   initial_node_count = 1
@@ -13,10 +30,11 @@ resource "google_container_cluster" "primary" {
   remove_default_node_pool = true
 }
 
-resource "google_container_node_pool" "primary_nodes" {
-  cluster  = google_container_cluster.primary.name
-  location = google_container_cluster.primary.location
-  name     = "${var.cluster_name}-node-pool"
+# Create the node pool for the first cluster
+resource "google_container_node_pool" "infra1_nodes" {
+  cluster  = google_container_cluster.infra1.name
+  location = google_container_cluster.infra1.location
+  name     = "${var.infra1_cluster_name}-node-pool"
 
   autoscaling {
     min_node_count = 1
@@ -24,8 +42,8 @@ resource "google_container_node_pool" "primary_nodes" {
   }
 
   node_config {
-    machine_type = "e2-standard-2"
-    disk_size_gb = 20
+    machine_type = "e2-medium" # Update to e2-medium as per your requirement
+    disk_size_gb = 10          # Set disk size to 10 GB
     disk_type    = "pd-standard"
     oauth_scopes = [
       "https://www.googleapis.com/auth/devstorage.read_only",
@@ -35,4 +53,54 @@ resource "google_container_node_pool" "primary_nodes" {
       "https://www.googleapis.com/auth/servicecontrol"
     ]
   }
+}
+
+# Create the second GKE cluster
+resource "google_container_cluster" "infra2" {
+  name     = var.infra2_cluster_name
+  location = var.region
+
+  initial_node_count = 1
+
+  node_config {
+    machine_type = "e2-medium"
+    disk_size_gb = 10
+    disk_type    = "pd-standard"
+  }
+
+  remove_default_node_pool = true
+}
+
+# Create the node pool for the second cluster
+resource "google_container_node_pool" "infra2_nodes" {
+  cluster  = google_container_cluster.infra2.name
+  location = google_container_cluster.infra2.location
+  name     = "${var.infra2_cluster_name}-node-pool"
+
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 3
+  }
+
+  node_config {
+    machine_type = "e2-medium" # Ensure consistency across clusters
+    disk_size_gb = 10          # Set disk size to 10 GB
+    disk_type    = "pd-standard"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/servicecontrol"
+    ]
+  }
+}
+
+# Optional: Outputs for cluster endpoints
+output "infra1_endpoint" {
+  value = google_container_cluster.infra1.endpoint
+}
+
+output "infra2_endpoint" {
+  value = google_container_cluster.infra2.endpoint
 }
